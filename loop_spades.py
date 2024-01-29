5b9e054e80fb3b6d28bf5f08bf6d388677abfd19
Tiago Belintani <113792806+TiagoBelintani@users.noreply.github.com>
2024-01-29 10:58:43 -0300

    loop_spades.py

--- loop_spades.py
+++ loop_spades.py
@@ -1,11 +1,18 @@
 import os
 
-arquivos_R1 = [f for f in os.listdir() if f.endswith('R1.fastq.gz')]
-arquivos_R2 = [f for f in os.listdir() if f.endswith('R2.fastq.gz')]
+# List all files ending with 'R1.fastq.gz' in the current directory
+files_R1 = [f for f in os.listdir() if f.endswith('R1.fastq.gz')]
 
-for arquivo_R1, arquivo_R2 in zip(arquivos_R1, arquivos_R2):
-    nome_saida = arquivo_R1.replace('_R1.fastq.gz', '')
-    comando = f'spades.py -1 {arquivo_R1} -2 {arquivo_R2} --careful --cov-cutoff auto -o {nome_saida}'
-    os.system(comando)
+# List all files ending with 'R2.fastq.gz' in the current directory
+files_R2 = [f for f in os.listdir() if f.endswith('R2.fastq.gz')]
 
+# Iterate over pairs of corresponding R1 and R2 files
+for file_R1, file_R2 in zip(files_R1, files_R2):
+    # Generate output name by removing '_R1.fastq.gz' from the R1 file name
+    output_name = file_R1.replace('_R1.fastq.gz', '')
 
+    # Construct the SPAdes command
+    command = f'spades.py -1 {file_R1} -2 {file_R2} --careful --cov-cutoff auto -o {output_name}'
+
+    # Execute the SPAdes command using os.system
+    os.system(command)
